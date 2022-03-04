8bec6a21a849760ef2a0c563db7c3fb58788dab1
Daniel Caicedo <daniel_caicedo@hotmail.com>
2022-03-04 17:16:25 -0500

    logout functionality added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -18,7 +19,10 @@ function App() {
         <Route path="/login">
           <LoginFormPage />
         </Route>
-        <Route>Splash Page</Route>
+        <Route path="/signup">
+          <SignupFormPage />
+        </Route>
+        {/* <Route>Splash Page</Route> */}
       </Switch>
     )
   );
