36da5fa5c648d4948777058340e43aa6f8ea9e19
Daniel Caicedo <daniel_caicedo@hotmail.com>
2022-03-10 12:59:47 -0500

    fixed the validation errors that now correctly show up

--- frontend/src/components/CreateStory/index.js
+++ frontend/src/components/CreateStory/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 // import { handleValidationErrors } from "../../../../backend/utils/validation";
@@ -30,15 +30,6 @@ const CreateStory = (stories) => {
   const updateTitle = (e) => setTitle(e.target.value);
   const updateBody = (e) => setBody(e.target.value);
 
-  useEffect(() => {
-    const validationErrors = [];
-    if (title.length < 4)
-      return validationErrors.push("Title must be more than 4 characters");
-    if (body.length < 4)
-      return validationErrors.push("Story must be more than 4 characters");
-    setErrors(validationErrors);
-  }, [title, body]);
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     const payload = {
@@ -49,21 +40,21 @@ const CreateStory = (stories) => {
     };
 
     //error validation
-    // setErrors([]);
+    setErrors([]);
 
-    // const newErrors = [];
+    const newErrors = [];
 
-    // if (payload.body.length < 4) {
-    //   newErrors.push("Story must be more than 4 characters");
-    // }
-    // if (payload.title.length < 4) {
-    //   newErrors.push("Title must be more than 4 characters");
-    // }
+    if (payload.body.length < 4) {
+      newErrors.push("Story must be more than 4 characters");
+    }
+    if (payload.title.length < 4) {
+      newErrors.push("Title must be more than 4 characters");
+    }
 
-    // if (newErrors.lenght > 0) {
-    //   setErrors(newErrors);
-    //   return;
-    // }
+    if (newErrors.length > 0) {
+      setErrors(newErrors);
+      return;
+    }
 
     dispatch(postStory(payload));
     history.push(`/stories/author/${userId}`);
